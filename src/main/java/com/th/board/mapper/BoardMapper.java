146b5196ec214d1a7237f9463ed8183cd33d4d5a
th <taehee.k102@gmail.com>
2023-04-06 23:56:12 +0900

    mybatis mapper xml 설정

--- src/main/java/com/th/board/mapper/BoardMapper.java
+++ src/main/java/com/th/board/mapper/BoardMapper.java
@@ -3,7 +3,6 @@ package com.th.board.mapper;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
 
 import com.th.board.vo.Board;
 
@@ -15,8 +14,6 @@ public interface BoardMapper {
 	 * 
 	 * @return List<Board>
 	 */
-	@Select("SELECT id, title, contents, reg_date, mod_date " +
-			"FROM board WHERE is_del = 0")
 	public List<Board> getBoardList();
 
 }
